75b344e35f42eb6e88eb4326673c7d1fa413381a
Wim Tanudjaja <tanudjaja@mac.com>
2024-09-16 08:36:10 -0400

    Initial Commit

---
+++ testChallenge/testChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testChallengeApp.swift
+//  testChallenge
+//
+//  Created by Wim Tanudjaja on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct testChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testChallengeTests/testChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  testChallengeTests.swift
+//  testChallengeTests
+//
+//  Created by Wim Tanudjaja on 9/16/24.
+//
+
+import Testing
+@testable import testChallenge
+
+struct testChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
